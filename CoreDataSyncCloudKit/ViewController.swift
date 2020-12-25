576d458ccef3e582b734a3d130a2a375ff171581
Danny Santoso <danny.sntoso@gmail.com>
2020-12-26 03:44:17 +0700

    Initial Commit

---
+++ CoreDataSyncCloudKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataSyncCloudKit
+//
+//  Created by danny santoso on 26/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
